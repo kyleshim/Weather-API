eadfa8421218e596f5436a8da9389b1e43dfab48
kyleshim <kyleshim@me.com>
2021-07-23 19:51:15 -0400

    First

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "0d2e76971d75e6a61108966cc9e41632"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB3fOBv5Fxb9IKiHXkhx5fVDfue2YDuix8"
